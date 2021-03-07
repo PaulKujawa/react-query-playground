598be6879751e10d2e92de754717e7d80bcbdc82
Paul Kujawa <p.kujawa@gmx.net>
2021-03-07 13:08:44 +0100

    Update index.tsx

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { QueryClientProvider } from "react-query";
-import "regenerator-runtime/runtime";
 import { App } from "./App";
 import "./index.css";
 import { ReactQueryClient } from "./lib";
